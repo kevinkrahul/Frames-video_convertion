99a8d23e0c76d04ec3192a4e747793e250c044e2
kevinkrahul <kevinrahul.22ec@kct.ac.in>
2023-09-22 00:03:08 +0530

    Initial commit

---
+++ frames-video.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+import re
+
+# Define the path to your frames and the output video file
+frames_folder = '/media/kevin/Ubuntu/Frames-video/Frames'
+output_video = 'output_video.mkv'
+
+# Get the list of frame filenames
+frame_files = [f for f in os.listdir(frames_folder) if f.endswith('.jpg')]  # Adjust the file extension if needed
+
+# Sort the frame filenames based on their numeric part (e.g., frame001.jpg, frame002.jpg, ...)
+frame_files.sort(key=lambda x: int(re.search(r'\d+', x).group()))
+
+# Get the dimensions of the first frame to set the video size
+first_frame = cv2.imread(os.path.join(frames_folder, frame_files[0]))
+height, width, _ = first_frame.shape
+
+# Define the codec for MKV format (use 'XVID' for MKV)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+# Create a VideoWriter object
+out = cv2.VideoWriter(output_video, fourcc, 30.0, (width, height))  # Adjust the frame rate (30.0) as needed
+
+# Iterate through frames and write them to the video
+for frame_file in frame_files:
+   frame = cv2.imread(os.path.join(frames_folder, frame_file))
+   out.write(frame)
+
+# Release the VideoWriter and close any open windows
+out.release()
+cv2.destroyAllWindows()
---
+++ video-frames.py
@@ -0,0 +1,34 @@
+import cv2
+import os
+
+# Path to the video file
+video_path = '/media/kevin/Ubuntu/Frames-video/[EMBER] Kimetsu no Yaiba - Mugen Ressha-hen - 07.mkv'
+
+# Output directory for frames
+output_dir = '/media/kevin/Ubuntu/Frames-video/Frames'
+
+# Create the output directory if it doesn't exist
+os.makedirs(output_dir, exist_ok=True)
+
+# Open the video file
+cap = cv2.VideoCapture(video_path)
+
+# Initialize a frame counter
+frame_count = 0
+
+# Read frames from the video and save them as image files
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    # Save the frame as an image file (e.g., frame_001.jpg, frame_002.jpg, ...)
+    frame_filename = os.path.join(output_dir, f'frame_{frame_count:03d}.jpg')
+    cv2.imwrite(frame_filename, frame)
+    
+    frame_count += 1
+
+# Release the video capture object
+cap.release()
+
+print(f'{frame_count} frames extracted and saved to {output_dir}')
